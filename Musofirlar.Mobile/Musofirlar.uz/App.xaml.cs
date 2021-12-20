4b803986180a5dba51653f5051a6f056df7cfe4d
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2021-12-20 19:15:00 +0500

    Mobile loyiha qo'shildi

---
+++ Musofirlar.Mobile/Musofirlar.uz/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Musofirlar.uz.Services;
+using Musofirlar.uz.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Musofirlar.uz
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
