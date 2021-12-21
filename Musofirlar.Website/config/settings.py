0b6ffc72cb25ff778c5f12a8ad73ca1fde4c5905
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2021-12-21 14:39:00 +0500

    collect static

--- Musofirlar.Website/config/settings.py
+++ Musofirlar.Website/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6zv#gzr-@6+**687rtumzs!t9d)=@lglb-oh=e(2up(1qi3yhv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 # Application definition
 
@@ -36,8 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # 3 rd party
 
     # local apps
     'accounts.apps.AccountsConfig',
